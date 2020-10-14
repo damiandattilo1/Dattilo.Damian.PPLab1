60a56a1290c0e645c31e290901c3b7b26af03be4
damiandattilo1 <60588905+damiandattilo1@users.noreply.github.com>
2020-10-14 11:26:22 -0300

    Add files via upload

---
+++ Primer parcial Laboratorio 1 Dattilo, Damian/trabajos.h
@@ -0,0 +1,77 @@
+#ifndef TRABAJOS_H_INCLUDED
+#define TRABAJOS_H_INCLUDED
+
+typedef struct
+{
+    int dia;
+	int mes;
+	int anio;
+}eFecha;
+
+typedef struct
+{
+	int id;
+	int idBici;
+	int idServicio;
+	eFecha fecha;
+	int isEmpty;
+}eTrabajo;
+
+
+#endif // TRABAJOS_H_INCLUDED
+
+
+/** \brief Inicializa un arreglo de trabajos
+ *
+ * \param arreglo
+ * \param tamaño del arreglo
+ * \return 1 si hubo un error, 0 si exitoso
+ *
+ */
+
+int inicializarTrabajos(eTrabajo trabajos[], int tam);
+
+/** \brief da de alta un elemento en un arreglo de trabajos
+ *
+ * \param arreglo
+ * \param tamaño del arreglo
+ * \return 1 si error, 0 si exitoso
+ *
+ */
+
+int altaTrabajo(eTrabajo trabajos[], int tamTrab, int idTrab, int max);
+
+/** \brief busca un elemento libre en un arreglo de trabajos
+ *
+ * \param arreglo
+ * \param tamaño del arreglo
+ * \return la posicion del elemento o -1 si arreglo completo
+ *
+ */
+
+int buscarTrabajoLibre(eTrabajo trabajos[], int tam);
+
+/** \brief imprime un arreglo de trabajos
+ *
+ * \param arreglo
+ * \param tamaño del arreglo
+ * \return -1 si error, o si OK
+ *
+ */
+
+int mostrarTrabajos(eTrabajo trabajos[], int tam);
+
+/** \brief imprime un elemento de un arreglo de trabajos
+ *
+ * \param elemento
+ *
+ */
+
+void mostrarTrabajo(eTrabajo unTrabajo);
+
+
+
+
+
+
+
